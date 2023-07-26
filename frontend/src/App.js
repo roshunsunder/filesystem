a27d4ff2226a70ce46f1aaa4ba24e07d03f58d93
Roshun Sunder <roshun.sunder@gmail.com>
2023-07-25 20:39:33 -0400

    Title slide up working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,27 @@ import "./App.css";
 
 const App = () => {
   const [inputText, setInputText] = useState("");
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFloating, setIsFloating] = useState(false);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleButtonClick = () => {
-    // Call your function here with the current inputText
-    console.log("Current input text:", inputText);
+    // Fake API call
+    setIsLoading(true);
+    setList([])
+    setIsLoading(false);
+    setIsFloating(true);
   };
 
   return (
+    //<div className={`app-container ${list.length > 0 ? "floating" : ""}`}>
     <div className="app-container">
-      <div className="title">LLM Filesystem</div>
-      <div className="input-container">
+      <div className={`title ${isFloating ? "floating" : ""}`}>LLM Filesystem</div>
+      <div className={`input-container ${isFloating ? "floating" : ""}`}>
         <input
           type="text"
           className="input-box"
@@ -24,10 +31,19 @@ const App = () => {
           onChange={handleInputChange}
           placeholder="Describe your file."
         />
-        <button className="go-button" onClick={handleButtonClick}>
-          Go
+        <button className="go-button" onClick={handleButtonClick} disabled={isLoading}>
+          {isLoading ? "Loading..." : "Go"}
         </button>
       </div>
+      {list.length > 0 && (
+        <div className="list-container">
+          {list.map((item, index) => (
+            <div key={index} className="list-item">
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
